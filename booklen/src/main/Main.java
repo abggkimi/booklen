159ad0a03a7b6f2e961240a78a081bed23ffab5c
abggkimi <a.miki.jp@gmail.com>
2026-09-08 14:05:50 +0900

    main.javaの構築

--- booklen/src/main/Main.java
+++ booklen/src/main/Main.java
@@ -1,11 +1,34 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		Scanner scan = new Scanner(System.in);
+		boolean b = true;
+
 		System.out.println("booklenへようこそ！");
 
+		while (b) {
+
+			System.out.println("番号を入力してください");
+			System.out.println("1：");
+
+			int sw = scan.nextInt();
+
+			switch (sw) {
+			case 1: {
+				b = false;
+				System.out.println("終了します...");
+				break;
+			}
+			default:
+				System.out.println("不正な値です入力し直してください");
+			}
+		}
+		scan.close();
 	}
 
 }
