ba4da8688201aec06fb7e8ac0994adac98486c4e
abggkimi <a.miki.jp@gmail.com>
2026-09-25 04:07:25 +0900

    BookのCURDの追加

--- booklen/src/main/Menu.java
+++ booklen/src/main/Menu.java
@@ -1,5 +1,89 @@
 package main;
 
+import java.util.Scanner;
+
+import service.BookService;
+
 public class Menu {
 
+	public static void rentMenu(Scanner scan) {
+		boolean bb = true;
+		System.out.println("貸し出し及び返却をします。");
+
+		while (bb) {
+
+			System.out.println("番号を入力してください");
+			System.out.println("1：貸し出し");
+			System.out.println("2：返却");
+			System.out.println("3：メインメニューに戻る");
+
+			int sw = scan.nextInt();
+
+			switch (sw) {
+			case 1: {
+				break;
+			}
+
+			case 2: {
+				break;
+			}
+
+			case 3: {
+				bb = false;
+				System.out.println("メインメニューに戻ります");
+				break;
+			}
+
+			default:
+				System.out.println("不正な値です入力し直してください");
+			}
+		}
+	}
+
+	public static void dataEdit(Scanner scan) {
+		boolean bb = true;
+		System.out.println("データを管理します");
+
+		while (bb) {
+
+			System.out.println("番号を入力してください");
+			System.out.println("1：本の追加");
+			System.out.println("2：本の詳細・変更");
+			System.out.println("3：ユーザーの登録");
+			System.out.println("4：ユーザー詳細・変更");
+			System.out.println("5：メインメニューへ戻る");
+
+			int sw = scan.nextInt();
+
+			switch (sw) {
+			case 1: {
+				BookService.addBook(scan);
+				break;
+			}
+
+			case 2: {
+				BookService.editBook(scan);
+				break;
+			}
+
+			case 3: {
+				break;
+			}
+
+			case 4: {
+				break;
+			}
+
+			case 5: {
+				bb = false;
+				System.out.println("メインメニューに戻ります");
+				break;
+			}
+
+			default:
+				System.out.println("不正な値です入力し直してください");
+			}
+		}
+
+	}
 }
--- booklen/src/service/UserService.java
+++ booklen/src/service/UserService.java
@@ -1,5 +1,17 @@
 package service;
 
-public class UserService {
+import java.util.Scanner;
 
+public class UserService {
+	public static void rent(Scanner scan) {
+		boolean b = true;
+		System.out.println("レンタル処理をします");
+
+		System.out.println("利用者番号を入力してください");
+		int userId = scan.nextInt();
+
+		System.out.println("貸し出しする書籍の書籍番号を入力してください");
+		int bookId = scan.nextInt();
+
+	}
 }
